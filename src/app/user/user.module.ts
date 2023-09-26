1b65e024d4ca166bb56cf9a2b74093871378d59e
Chris Lopez <44450518+loChris@users.noreply.github.com>
2023-09-26 17:01:41 -0400

    moved code around for the parent modal module, and moved log in modal code into a separate component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,7 +4,6 @@ import { SharedModule } from '../shared/shared.module';
 
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
-
 @NgModule({
   declarations: [
     AuthModalComponent
