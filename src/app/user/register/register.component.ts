250aea22b3f8c9bfc897709dec53139a13d9760e
Chris Lopez <44450518+loChris@users.noreply.github.com>
2023-10-17 18:59:29 -0400

    added validation to the register form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,11 +9,36 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 export class RegisterComponent {
   registerForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    passwordConfirmation: new FormControl(''),
-    phoneNumber: new FormControl(''),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    age: new FormControl('', [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(150),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    passwordConfirmation: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
   });
 }
