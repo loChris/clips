815ea703a4cc96915345619be745490a05784838
Chris Lopez <44450518+loChris@users.noreply.github.com>
2023-10-18 12:39:39 -0400

    cleaned up register name input and moved it into its own component

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -4,41 +4,47 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-
 export class RegisterComponent {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(50),
+  ]);
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email,
+    Validators.minLength(3),
+    Validators.maxLength(50),
+  ]);
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(150),
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(50),
+  ]);
+  passwordConfirmation = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(50),
+  ]);
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(50),
+  ]);
+
   registerForm = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-      Validators.minLength(2),
-      Validators.maxLength(50),
-    ]),
-    email: new FormControl('', [
-      Validators.required,
-      Validators.email,
-      Validators.minLength(3),
-      Validators.maxLength(50),
-    ]),
-    age: new FormControl('', [
-      Validators.required,
-      Validators.min(18),
-      Validators.max(150),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(50),
-    ]),
-    passwordConfirmation: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(50),
-    ]),
-    phoneNumber: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(50),
-    ]),
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    passwordConfirmation: this.passwordConfirmation,
+    phoneNumber: this.phoneNumber,
   });
 }
