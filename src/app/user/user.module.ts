d86382bc8380fef7daf0bf946170b2ac3e3878e7
Chris Lopez <44450518+loChris@users.noreply.github.com>
2023-10-19 14:23:24 -0400

    added validtaion for the login email input

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
@@ -8,19 +8,8 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-    AuthModalComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    SharedModule
-  ],
-  exports: [
-    AuthModalComponent,
-  ]
+  declarations: [AuthModalComponent, LoginComponent, RegisterComponent],
+  imports: [CommonModule, ReactiveFormsModule, SharedModule, FormsModule],
+  exports: [AuthModalComponent],
 })
-
-export class UserModule { }
+export class UserModule {}
